f85294d3c358f2f23de090558f40f65e3ee29de3
eladdee <eladevrim65@gmail.com>
2025-03-11 15:11:31 +0300

    sgdsg
    dgdg

--- iframe.php
+++ iframe.php
@@ -42,6 +42,7 @@
                         <a href="/tr/casino/cat/jojobet'e-ozel" data-original-href="/casino/cat/jojobet'e-ozel"><img class="lazy" src="/stateless-jojobet/2019/07/ca8c5118-blackjack.gif" alt="" width="320" height="80"></a><br>
                         <a href="/tr/sponsorluklar" data-original-href="/sponsorluklar"><img class="lazy" src="/stateless-jojobet/2019/07/8fada16a-sponsorluklar.gif" alt="" width="320" height="80"></a><br>
                         <a href="https://t.me/jojobettelegramdestekbot" target="_blank" data-original-href="https://t.me/jojobettelegramdestekbot"><img class="lazy" src="/stateless-jojobet/2019/07/c7803416-solblok-tlgram.gif" alt="" width="320" height="80"></a><br>
+                        <a href="https://heylink.me/holiguncel/" target="_blank" data-original-href="https://heylink.me/holiguncel/"><img src="https://www.holiganbet1085.com/stateless-holiganbet-one/2022/01/cb14bf1b-solsosyal1-1.png" alt="" width="320" height="40"></a>
                         <a href="https://www.holiganbet1085.com/tr/sports/i/spor/esports/96/tümü/0/disiplin/canlı" data-original-href="/sports/i/spor/esports/96/tümü/0/disiplin/canlı"><img src="https://othercdn.com/holiganbet/solstun/espor-gif.gif" alt="" width="320" height="80"></a>
                      </p>
                   </div>
