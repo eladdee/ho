a01512ac54cdba7a0077e13f3d66fc27113fa191
eladdee <eladevrim65@gmail.com>
2025-03-19 14:21:43 +0300

    rhdrh
    dhd

--- view/aviator.php
+++ view/aviator.php
@@ -1,102 +1,99 @@
 <section class="SectionContent">
    <div class="ComponentLayout InstanceOperatorGamepageDesktopLayout Layout MinimizedContainer">
       <div class="GameRightWrapper ComponentLayout InstanceOperatorGamepageRightCol Layout NoGameLeftWrapper">
-         <div class="GmowContainer" style="width: auto;">
-            <a class="ComponentAnchor InstanceBackButton Button Anchor" href="/tr/casino/cat/pragmatic-casino">
-               <span class="ComponentIcon InstanceOperatorClose ButtonIconStart Icon">
-                  <span class="SvgIcon MainIcon">
-                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                        <path d="M13.414 12l4.95-4.95-1.414-1.414-4.95 4.95-4.95-4.95L5.636 7.05l4.95 4.95-4.95 4.95 1.414 1.414 4.95-4.95 4.95 4.95 1.414-1.414-4.95-4.95z"></path>
+      <div class="GmowContainer" style="width: auto;">
+   <a class="ComponentAnchor InstanceBackButton Button Anchor" href="/tr/casino/cat/pragmatic-casino">
+      <span class="ComponentIcon InstanceOperatorClose ButtonIconStart Icon">
+         <span class="SvgIcon MainIcon">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+               <path d="M13.414 12l4.95-4.95-1.414-1.414-4.95 4.95-4.95-4.95L5.636 7.05l4.95 4.95-4.95 4.95 1.414 1.414 4.95-4.95 4.95 4.95 1.414-1.414-4.95-4.95z"></path>
+            </svg>
+         </span>
+      </span>
+   </a>
+   <ul class="GmowGameRow SingleGames">
+      <li class="GmowGameColumn" style="width: auto;">
+         <div class="GmowGameWindow">
+            <h2 class="ComponentTitle InstanceCasinGameTitle Title">Aviator</h2>
+            <div class="IframeContainer GmowGame GmowOperatorDesktop GmowGameFrame" style="width: 1086px; height: 646.979px;">
+               <div class="BtnGroup LeftSide">
+                  <button type="button" class="BtnGroupItem SwitchButton Button" title="Oyun Değiştir">
+                     <span class="ComponentIcon Icon">
+                        <span class="SvgIcon MainIcon">
+                           <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40">
+                              <path fill="#CACACA" d="M14.858 27.5v-5l-9 7 9 7v-5h20.001v-4zM34.859 10.5l-9 7v-14l9 7zm-29.001 2v-4h20.001v4H5.858z"></path>
+                           </svg>
+                        </span>
+                     </span>
+                  </button>
+               </div>
+               <div class="GameIframeWrapper"><iframe src="https://gamelaunch.holiganbet1087.com/Loader/Start/2218/aviator-spribe?language=tr&amp;funMode=false&amp;_sid=&amp;casinolobbyurl=https://www.holiganbet1087.com/tr/casino" title="Aviator" class="GameIframe" allowfullscreen="" data-width="940" data-height="560" width="1086" height="646.9787234042553"></iframe></div>
+            </div>
+         </div>
+      </li>
+   </ul>
+   <div class="GmowDetails">
+      <div class="GmowDetailsRow">
+         <div class="GmowDetailsInfo">
+            <div class="GmowDetailsInfoButton"><button type="button" class="Button ButtonLogin " id="UnauthenticatedGamePage-Aviator" data-disable-click="false"><span class="AnchorText" id="UnauthenticatedGamePage-Aviator-Text">Giriş Yap</span></button><a class="ComponentAnchor Button ButtonRegister ComponentAnchor Anchor  Anchor" id="RegisterButton-UnauthenticatedGamePage-Aviator" href="/tr/register"><span class="AnchorText" id="RegisterButton-UnauthenticatedGamePage-Aviator-Text">Kayıt Ol</span></a></div>
+         </div>
+         <div class="GmowDetailsActions">
+            <button type="button" class="Button GmowBottomIcon Selected" id="ScreenSplitButton-Single-Aviator">
+               <span class="ComponentIcon ButtonIconStart Icon" id="ScreenSplitButton-Single-Aviator-IconWrapper">
+                  <span class="SvgIcon MainIcon" id="ScreenSplitButton-Single-Aviator-Icon">
+                     <svg xmlns="http://www.w3.org/2000/svg" width="40" height="32" viewBox="0 0 40 32">
+                        <rect rx="1" height="32" width="40" fill="#fff"></rect>
+                     </svg>
+                  </span>
+               </span>
+            </button>
+            <button type="button" class="Button GmowBottomIcon " id="ScreenSplitButton-Dual-Aviator">
+               <span class="ComponentIcon ButtonIconStart Icon" id="ScreenSplitButton-Dual-Aviator-IconWrapper">
+                  <span class="SvgIcon MainIcon" id="ScreenSplitButton-Dual-Aviator-Icon">
+                     <svg xmlns="http://www.w3.org/2000/svg" width="40" height="32" viewBox="0 0 40 32">
+                        <rect rx="1" height="32" width="18" x="21.95" fill="#fff"></rect>
+                        <rect rx="1" height="32" width="18" fill="#fff"></rect>
+                     </svg>
+                  </span>
+               </span>
+            </button>
+            <button type="button" class="Button GmowBottomIcon " id="ScreenSplitButton-Quad-Aviator">
+               <span class="ComponentIcon ButtonIconStart Icon" id="ScreenSplitButton-Quad-Aviator-IconWrapper">
+                  <span class="SvgIcon MainIcon" id="ScreenSplitButton-Quad-Aviator-Icon">
+                     <svg xmlns="http://www.w3.org/2000/svg" width="40" height="32" viewBox="0 0 40 32">
+                        <rect rx="1" height="14" width="18" y="17.986" x="21.95" fill="#fff"></rect>
+                        <rect rx="1" height="14" width="18" y="17.986" fill="#fff"></rect>
+                        <rect rx="1" height="14" width="18" fill="#fff"></rect>
+                        <rect rx="1" height="14" width="18" x="21.95" fill="#fff"></rect>
+                     </svg>
+                  </span>
+               </span>
+            </button>
+            <a class="ComponentAnchor ComponentButton ButtonMinimize Button GmowBottomIcon Anchor" href="/tr/casino/cat/pragmatic-casino">
+               <span class="ComponentIcon Icon">
+                  <span class="SvgIcon MainIcon CustomHtml">
+                     <svg xmlns="http://www.w3.org/2000/svg" width="29" height="29" viewBox="0 0 29 29">
+                        <g stroke="#fff" fill="none" fill-rule="evenodd">
+                           <rect stroke-width="1.669" x=".89" y=".834" width="27.158" height="27.331" rx="2.225"></rect>
+                           <path d="M5.5 23.5l8-8m-2 7.75H5.25V17" stroke-width="1.5"></path>
+                        </g>
                      </svg>
                   </span>
                </span>
             </a>
-            <ul class="GmowGameRow SingleGames">
-               <li class="GmowGameColumn" style="width: auto;">
-                  <div class="GmowGameWindow">
-                     <h2 class="ComponentTitle InstanceCasinGameTitle Title">Aviator</h2>
-                     <div class="IframeContainer GmowGame GmowOperatorDesktop GmowGameFrame" style="width: 1008px; height: 600.511px;">
-                        <div class="BtnGroup LeftSide">
-                           <button type="button" class="BtnGroupItem SwitchButton Button" title="Oyun Değiştir">
-                              <span class="ComponentIcon Icon">
-                                 <span class="SvgIcon MainIcon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40">
-                                       <path fill="#CACACA" d="M14.858 27.5v-5l-9 7 9 7v-5h20.001v-4zM34.859 10.5l-9 7v-14l9 7zm-29.001 2v-4h20.001v4H5.858z"></path>
-                                    </svg>
-                                 </span>
-                              </span>
-                           </button>
-                        </div>
-                        <div class="GameIframeWrapper"><iframe src="https://gamelaunch.holiganbet1087.com/Loader/Start/2218/aviator-spribe?language=tr&amp;funMode=false&amp;_sid=&amp;casinolobbyurl=https://www.holiganbet1087.com/tr/casino" title="Aviator" class="GameIframe" allowfullscreen="" data-width="940" data-height="560" width="1008" height="600.5106382978723"></iframe></div>
-                     </div>
-                  </div>
-               </li>
-            </ul>
-            <div class="GmowDetails">
-               <div class="GmowDetailsRow">
-                  <div class="GmowDetailsInfo">
-                     <div class="GmowDetailsInfoButton"><button type="button" class="Button ButtonLogin " id="UnauthenticatedGamePage-Aviator" data-disable-click="false"><span class="AnchorText" id="UnauthenticatedGamePage-Aviator-Text">Giriş Yap</span></button><a class="ComponentAnchor Button ButtonRegister ComponentAnchor Anchor  Anchor" id="RegisterButton-UnauthenticatedGamePage-Aviator" href="/tr/register"><span class="AnchorText" id="RegisterButton-UnauthenticatedGamePage-Aviator-Text">Kayıt Ol</span></a></div>
-                  </div>
-                  <div class="GmowDetailsActions">
-                     <button type="button" class="Button GmowBottomIcon Selected" id="ScreenSplitButton-Single-Aviator">
-                        <span class="ComponentIcon ButtonIconStart Icon" id="ScreenSplitButton-Single-Aviator-IconWrapper">
-                           <span class="SvgIcon MainIcon" id="ScreenSplitButton-Single-Aviator-Icon">
-                              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="32" viewBox="0 0 40 32">
-                                 <rect rx="1" height="32" width="40" fill="#fff"></rect>
-                              </svg>
-                           </span>
-                        </span>
-                     </button>
-                     <button type="button" class="Button GmowBottomIcon " id="ScreenSplitButton-Dual-Aviator">
-                        <span class="ComponentIcon ButtonIconStart Icon" id="ScreenSplitButton-Dual-Aviator-IconWrapper">
-                           <span class="SvgIcon MainIcon" id="ScreenSplitButton-Dual-Aviator-Icon">
-                              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="32" viewBox="0 0 40 32">
-                                 <rect rx="1" height="32" width="18" x="21.95" fill="#fff"></rect>
-                                 <rect rx="1" height="32" width="18" fill="#fff"></rect>
-                              </svg>
-                           </span>
-                        </span>
-                     </button>
-                     <button type="button" class="Button GmowBottomIcon " id="ScreenSplitButton-Quad-Aviator">
-                        <span class="ComponentIcon ButtonIconStart Icon" id="ScreenSplitButton-Quad-Aviator-IconWrapper">
-                           <span class="SvgIcon MainIcon" id="ScreenSplitButton-Quad-Aviator-Icon">
-                              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="32" viewBox="0 0 40 32">
-                                 <rect rx="1" height="14" width="18" y="17.986" x="21.95" fill="#fff"></rect>
-                                 <rect rx="1" height="14" width="18" y="17.986" fill="#fff"></rect>
-                                 <rect rx="1" height="14" width="18" fill="#fff"></rect>
-                                 <rect rx="1" height="14" width="18" x="21.95" fill="#fff"></rect>
-                              </svg>
-                           </span>
-                        </span>
-                     </button>
-                     <a class="ComponentAnchor ComponentButton ButtonMinimize Button GmowBottomIcon Anchor" href="/tr/casino/cat/pragmatic-casino">
-                        <span class="ComponentIcon Icon">
-                           <span class="SvgIcon MainIcon CustomHtml">
-                              <svg xmlns="http://www.w3.org/2000/svg" width="29" height="29" viewBox="0 0 29 29">
-                                 <g stroke="#fff" fill="none" fill-rule="evenodd">
-                                    <rect stroke-width="1.669" x=".89" y=".834" width="27.158" height="27.331" rx="2.225"></rect>
-                                    <path d="M5.5 23.5l8-8m-2 7.75H5.25V17" stroke-width="1.5"></path>
-                                 </g>
-                              </svg>
-                           </span>
-                        </span>
-                     </a>
-                     <button class="ComponentButton ButtonFullScreen Button" type="button">
-                        <span class="ComponentIcon ButtonIconStart Icon">
-                           <span class="SvgIcon MainIcon">
-                              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40">
-                                 <path d="M7.5 31.44l6.54-6.54 1.06 1.06-6.54 6.54H13V34H6v-7h1.5v4.44zm23.94 1.06l-6.54-6.54 1.06-1.06 6.54 6.54V27H34v7h-7v-1.5h4.44zM32.5 8.56l-6.54 6.54-1.06-1.06 6.54-6.54H27V6h7v7h-1.5V8.56zM8.56 7.5l6.54 6.54-1.06 1.06L7.5 8.56V13H6V6h7v1.5H8.56z"></path>
-                              </svg>
-                           </span>
-                        </span>
-                     </button>
-                  </div>
-               </div>
-               <div class="GmowDetailsClock">
-                  <div class="ComponentClock">14:15:52</div>
-               </div>
-            </div>
+            <button class="ComponentButton ButtonFullScreen Button" type="button">
+               <span class="ComponentIcon ButtonIconStart Icon">
+                  <span class="SvgIcon MainIcon">
+                     <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40">
+                        <path d="M7.5 31.44l6.54-6.54 1.06 1.06-6.54 6.54H13V34H6v-7h1.5v4.44zm23.94 1.06l-6.54-6.54 1.06-1.06 6.54 6.54V27H34v7h-7v-1.5h4.44zM32.5 8.56l-6.54 6.54-1.06-1.06 6.54-6.54H27V6h7v7h-1.5V8.56zM8.56 7.5l6.54 6.54-1.06 1.06L7.5 8.56V13H6V6h7v1.5H8.56z"></path>
+                     </svg>
+                  </span>
+               </span>
+            </button>
          </div>
       </div>
+      <div class="GmowDetailsClock">
+         <div class="ComponentClock">14:21:21</div>
+      </div>
    </div>
-</section>
+</div>
